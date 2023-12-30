1eb72733d7af4e2b50683e28934a47c3ef11cf61
Ogbonna Basil <mrcea@Ogbonna.local>
2023-12-30 16:49:25 +0200

    update values link

--- flink/releases/install-flink.sh
+++ flink/releases/install-flink.sh
@@ -7,7 +7,7 @@ helm repo update
 echo "Install flink"
 helm upgrade \
     --install \
-    --values flink-values.yml \
+    --values _one-acre-fund.airflow/releases/flink-values.yml \
     --namespace bi \
     --timeout 10m \
     --debug \
